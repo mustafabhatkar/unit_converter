eb25db291cd3ea6aa7c57f21a0e4630cef279496
mustafabhatkar <mustafabhatkar@gmail.com>
2021-09-10 22:02:08 +0530

    Added GridView in CategoryPage

--- lib/category/category_page.dart
+++ lib/category/category_page.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:unit_converter/utils/constants.dart';
 
 class CategoryPage extends StatelessWidget {
-  const CategoryPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(Constants.categoryTitle),
         ),
+        body: Container(
+            child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1/2,
+          children: <Widget>[
+            Container(color:Colors.red),
+            Container(color:Colors.blue),
+            Container(color:Colors.green),
+            Container(color:Colors.purple),
+          ],
+        )),
         floatingActionButton: FloatingActionButton.extended(
-            onPressed: () =>Navigator.pop(context),
+            onPressed: () => Navigator.pop(context),
             label: Text("Back"),
             icon: Icon(Icons.arrow_back)));
   }
