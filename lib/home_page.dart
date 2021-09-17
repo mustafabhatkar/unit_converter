f1a20bd25ca70bd7b27faa059d24e4d3171e0c08
mustafabhatkar <mustafabhatkar@gmail.com>
2021-09-17 22:26:09 +0530

    Started Designing Home Page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:unit_converter/category/category_page.dart';
 import 'package:unit_converter/utils/constants.dart';
 
@@ -10,6 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List unitsList = ["cm", "m", "mm"];
+  var fromUnit = "cm";
   var categoryName = "";
   @override
   Widget build(BuildContext context) {
@@ -17,15 +20,57 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text(Constants.appName),
         ),
-        body: Center(child: Container(child: Text(categoryName))),
+        body: Center(
+            child: Container(
+                margin: EdgeInsets.all(8.0),
+                child: Column(children: [
+                  TextField(
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.allow(
+                          RegExp(r'^\d*\.?\d{0,9}')),
+                    ],
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        hintText: 'Enter Your Number'),
+                  ),
+                  Container(
+                    height: 50.0,
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: unitsList.length,
+                        itemBuilder: (context, index) {
+                          String currentUnit = unitsList[index];
+                          return InkWell(
+                            onTap:(){
+                              setState(() {
+                                fromUnit = currentUnit;
+                              });
+                            },
+                            child: Container(
+                                margin: EdgeInsets.all(8.0),
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 12.0),
+                                decoration: BoxDecoration(
+                                    color: fromUnit == currentUnit
+                                        ? Colors.grey.shade300
+                                        : null,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8.0)),
+                                    border: Border.all()),
+                                child: Center(child: Text(currentUnit))),
+                          );
+                        }),
+                  )
+                ]))),
         floatingActionButton: FloatingActionButton(
             onPressed: () async {
               categoryName = await Navigator.push(context,
                   MaterialPageRoute(builder: (context) => CategoryPage()));
-                  setState(() {
-
-                  });
-
+              setState(() {});
             },
             child: Icon(Icons.add)));
   }
